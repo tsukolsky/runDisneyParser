4a241b0559b1c6158feb28bf32638ac4aff93814
Todd Sukolsky <tsukolsky@gmail.com>
2025-01-21 11:48:52 -0500

    added parsing for goofy

--- runDisneyDopeyParser.py
+++ runDisneyDopeyParser.py
@@ -1,4 +1,4 @@
-import re, sys, argparse
+import re, sys, argparse, time
 
 DEBUG = False
 
@@ -20,8 +20,8 @@ class Partcipant():
         self.line = line
 
         # Break the line into three parts - whats before the times, what's after, and the times themselves
-        firstPos = self.__GetFirstTimePosition(line)
-        lastPos = self.__GetLastTimePosition(line)
+        firstPos = self.GetFirstTimePosition(line)
+        lastPos = self.GetLastTimePosition(line)
         preamble = line[:firstPos]
         postamble = line[lastPos:]
         self.stringTime = line[firstPos:lastPos]
@@ -31,9 +31,8 @@ class Partcipant():
         self.ParseTimes(self.stringTime)
         self.origin = postamble.strip().strip(',')
 
-
     def __str__(self):
-        return f"{self.line}\nPartcipant: {self.name} {self.age} {self.sex} {self.origin}\n{self.stringTime}\n{self._times}"
+        return f"{self.line}\nParticipant: {self.name} {self.age} {self.sex} {self.origin}\n{self.stringTime}\n{self._times}"
 
     def ParsePreamble(self, preamble):
         # Preamble should have spaces inbetween values, some will be None or empty as well
@@ -45,7 +44,7 @@ class Partcipant():
         self.age = subfields[-2].strip().strip(',')
         self.sex = subfields[-1].strip().strip(',')
 
-    def __ConvertToSeconds(self, timestring : str) -> int:
+    def ConvertToSeconds(self, timestring : str) -> int:
         # Takes in time in hh:mm:ss format, or mm:ss format if it's a respectable 5k/10k time, and returns seconds
         fields = timestring.split(':')
         totalTime = int(fields[-1]) + int(fields[-2])*60
@@ -53,7 +52,7 @@ class Partcipant():
             totalTime += int(fields[0])*60*60
         return totalTime
 
-    def __GetFirstTimePosition(self, middleString) -> int:
+    def GetFirstTimePosition(self, middleString) -> int:
         # Get location of first time - this could be a 5k/10k split, so it might only have one semicolon
         # TODO: Unhandled error here if there is no start_pos, but that should have been cleared by earlier parse
         # to validate there is enough times
@@ -69,7 +68,7 @@ class Partcipant():
             print(f"Error parsing string {self.lastname}, full {self.line}, middle {middleString}")
         return start_pos
 
-    def __GetLastTimePosition(self, middleString) -> int:
+    def GetLastTimePosition(self, middleString) -> int:
         # Get location of last time - this should be a marathon time, so three objects in hh:mm:ss
         # TODO: Unhandled error here if there is no end_pos, but that should have been cleared by earlier parse
         # to validate there is enough times
@@ -86,10 +85,54 @@ class Partcipant():
             return 0
 
     def ParseTimes(self, times : str):
+        print("Base class call")
+        self.result = {FIVEK : 0, TENK : 0, HALF : 0, FULL : 0, TOTAL_TIME : 0}
+
+    def GetCSVLine(self, separator : str) -> str:
+        print("Base class call")
+        return ""
+
+class GoofyParticipant(Partcipant):
+    def ParseTimes(self, times : str):
+        # Take the time string and parse out all the times. Goofy has clock/net/clock/net, so grab #2 and #4 (1,3)
+        individualResult = {HALF : 0, FULL : 0, TOTAL_TIME : 0}
+        pattern = r"(\d+):(\d+):(\d+)"
+        timeStr = times
+        res = re.split(pattern, timeStr)
+        matches = list(re.finditer(pattern, timeStr))
+        if matches:
+            last_match = matches[-1]
+            start_pos = last_match.start()
+            end_pos = last_match.end()
+            times = timeStr[0:end_pos].split(" ")
+        else:
+            # no match, meaning it isn't there. this should never happen based on previous checks
+            raise("error - unknown value in string field")
+            pass
+
+        numSec = self.ConvertToSeconds(times[-1])
+        individualResult[FULL] = numSec
+        numSec = self.ConvertToSeconds(times[-3])
+        individualResult[HALF] = numSec
+
+        # Save the final result and tally up their total time
+        self.result = individualResult
+        self.result[TOTAL_TIME] = self.result[HALF] + self.result[FULL]
+
+    def GetCSVLine(self, separator : str) -> str:
+        dataList = [self.name, self.age, self.sex, self.origin, self.result[HALF], self.result[FULL], self.result[TOTAL_TIME]]
+        ostring = ""
+        for item in dataList[:-1]:
+            ostring += f"{item}{separator}"
+        ostring += f"{dataList[-1]}"
+        return ostring
+
+class DopeyParticipant(Partcipant):
+    def ParseTimes(self, times : str):
         # Take the time string and parse out all the times. Based on trackshack's PDF, there is always a marathon
-        # or half marathon. Becuase we want to get Dopey results (original intent), we want only racers with 
+        # or half marathon. Becuase we want to get Dopey results (original intent), we want only racers with
         # these last two larger times in their results. So assume* they are always there, and if one is missing
-        # it's the 5k 
+        # it's the 5k
         individualResult = {FIVEK : 0, TENK : 0, HALF : 0, FULL : 0, TOTAL_TIME : 0}
         pattern = r"(\d+):(\d+):(\d+)"
         timeStr = times
@@ -106,27 +149,36 @@ class Partcipant():
             pass
 
         # Go backwards through the list, assuming full and half are ones always written in PDF
-        # Why? Because in 2025 people had their bibs not working for the 5k or 10k, but were fixed in 
+        # Why? Because in 2025 people had their bibs not working for the 5k or 10k, but were fixed in
         # half and full. This script does not accound for these issues completely. Excel is your friend
         # for that
         if len(times) > 0:
-            numSec = self.__ConvertToSeconds(times[-1])
+            numSec = self.ConvertToSeconds(times[-1])
             individualResult[FULL] = numSec
         if len(times) > 1:
-            numSec = self.__ConvertToSeconds(times[-2])
+            numSec = self.ConvertToSeconds(times[-2])
             individualResult[HALF] = numSec
         if len(times) > 2:
-            numSec = self.__ConvertToSeconds(times[-3])
+            numSec = self.ConvertToSeconds(times[-3])
             individualResult[TENK] = numSec
         if len(times) > 3:
-            numSec = self.__ConvertToSeconds(times[-4])
+            numSec = self.ConvertToSeconds(times[-4])
             individualResult[FIVEK] = numSec
 
         # Save the final result and tally up their total time
         self.result = individualResult
         self.result[TOTAL_TIME] = self.result[FIVEK] + self.result[TENK] + self.result[HALF] + self.result[FULL]
 
-def ParsePDF(filename : str, outputfile : str = "tmp-markup.txt"):
+    def GetCSVLine(self, separator : str) -> str:
+        dataList = [self.name, self.age, self.sex, self.origin, self.result[FIVEK], self.result[TENK],
+                    self.result[HALF], self.result[FULL], self.result[TOTAL_TIME]]
+        ostring = ""
+        for item in dataList[:-1]:
+            ostring += f"{item}{separator}"
+        ostring += f"{dataList[-1]}"
+        return ostring
+
+def ParsePDF(filename : str, outputfile : str = None):
     # Using pyupdf4llm, parse a PDF and get the text lines in the document/file
     try:
         import pymupdf4llm as pdf
@@ -135,7 +187,7 @@ def ParsePDF(filename : str, outputfile : str = "tmp-markup.txt"):
         return []
 
     md_text = pdf.to_markdown(filename)
-    if DEBUG:
+    if outputfile is not None:
         with open(outputfile, 'w') as ofh:
             num = ofh.write(md_text)
     lines = md_text.split('\n')
@@ -144,13 +196,18 @@ def ParsePDF(filename : str, outputfile : str = "tmp-markup.txt"):
 def SetupArgparse():
     # Setup Argparse for runDisney program - you can choose to evaluate a pdf or markdown file
     parser = argparse.ArgumentParser(prog=__doc__, description="Parser for results data provided by TrackShack of runDisney Dopey Challenge. Exports a CSV for import into excel for further data analysis")
-    parser.add_argument('-t', '--times', type=int, help='Minimum number of times to look for.', default=3)
-    parser.add_argument('-o', '--output', type=str, help='Output file for excel formatted data', default="output.csv")
-    parser.add_argument('-d', '--delimiter', type=str, help='delimiter of data for csv file. default is semicolon \';\'', default=';')
+    parser.add_argument('-t', '--times', type=int, help='Minimum number of times to look for.', default=2)
+    parser.add_argument('-o', '--output', type=str, help='Output file for excel formatted data', default="output/output.csv")
+    parser.add_argument('-s', '--separater', type=str, help='separater of data for csv file. default is semicolon \';\'', default=';')
+    parser.add_argument('-e', '--export', type=str, help="Export markdown of PDF parse to file specificed", default=None)
 
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-m', '--markdown', type=str, help='Markdown file to parse')
     group.add_argument('-p','--pdf', type=str, help='Input PDF')
+
+    dgroup = parser.add_mutually_exclusive_group(required=True)
+    dgroup.add_argument('-g', '--goofy', action="store_true", default=False, help = "Goofy parse")
+    dgroup.add_argument('-d', '--dopey', action="store_true", default= False, help = "Dopey parse")
     return parser
 
 if __name__ == "__main__":
@@ -167,7 +224,7 @@ if __name__ == "__main__":
             sys.exit(-1)
     else:
         print(f"Converting PDF {args.pdf} to data")
-        lines = ParsePDF(args.pdf)
+        lines = ParsePDF(args.pdf, args.export)
     print(f"Grabbed {len(lines)} lines to parse")
 
     # Open output file and start parsing the input data
@@ -180,10 +237,10 @@ if __name__ == "__main__":
         matches = re.findall(pattern, line)
         count = len(matches)
         if count >= args.times:
-            part = Partcipant(line)
-            dataList = [part.name, part.age, part.sex, part.origin, part.result[FIVEK], part.result[TENK], part.result[HALF], part.result[FULL], part.result[TOTAL_TIME]]
-            for item in dataList[:-1]:
-                ofh.write(f"{item}{args.delimiter}")
-            ofh.write(f"{dataList[-1]}\n")
+            if args.dopey:
+                part = DopeyParticipant(line)
+            else:
+                part = GoofyParticipant(line)
+            ofh.write(part.GetCSVLine(args.separater)+"\n")
     ofh.close()
     print(f"Successfully exported data to csv")
